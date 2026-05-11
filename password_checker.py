adbdf1f2f22a10d3caa6af6787cf781ea9d2ad86
parajyothsna199 <parajyothsna199@gmail.com>
2026-05-11 18:51:32 +0530

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,1 +1,26 @@
 
+import re
+
+def check_password(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 2
+    if re.search("[A-Z]", password):
+        score += 1
+    if re.search("[a-z]", password):
+        score += 1
+    if re.search("[0-9]", password):
+        score += 1
+    if re.search("[!@#$%^&*]", password):
+        score += 2
+
+    if score <= 3:
+        return "Weak ❌"
+    elif score <= 6:
+        return "Medium ⚠️"
+    else:
+        return "Strong ✅"
+
+pwd = input("Enter password: ")
+print(check_password(pwd))
